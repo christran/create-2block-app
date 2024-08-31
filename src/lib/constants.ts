5d71ff2ce9d49bc7166b6c142c294904234723ca
Chris Tran <christophertran714@gmail.com>
2024-08-30 17:40:16 -0700

    initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const APP_TITLE = "✌️BLOCK";
+export const DATABASE_PREFIX = "2block";
+export const TEST_DB_PREFIX = "test_2block";
+export const EMAIL_SENDER = '"✌️BLOCK" <hello@2block.co>';
+
+export enum Paths {
+  Home = "/",
+  Login = "/login",
+  Signup = "/signup",
+  Dashboard = "/dashboard",
+  VerifyEmail = "/verify-email",
+  ResetPassword = "/reset-password",
+}
