f2be03f93145b3fe52327c23029fe075acb36868
Chris Tran <christophertran714@gmail.com>
2024-09-16 07:46:48 -0700

    fix nextjs cdn caching

--- next.config.js
+++ next.config.js
@@ -8,6 +8,35 @@ const config = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+ 
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "2block.94b3308d853c120b7b7d21af8dc272e6.r2.cloudflarestorage.com",
+          },
+        ],
+        unoptimized: true,
+    },
+
+    async headers() {
+        if (process.env.NODE_ENV !== 'production') {
+            return [];
+        }
+    
+        return [
+            {
+                source: '/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2)',
+                locale: false,
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000',
+                    }
+                ],
+            }
+        ];
+    },
 
     // Docker Builds
     // output: 'standalone',
