0fa4683f0c957fa2db1d8a312e3bf64cbb2c497f
Chris Tran <christophertran714@gmail.com>
2024-09-18 20:22:50 -0700

    disable image optimization for now

--- next.config.js
+++ next.config.js
@@ -20,8 +20,7 @@ const config = {
             hostname: "media.2block.co",
           },
         ],
-        // deviceSizes: [600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        unoptimized: false,
+        unoptimized: true, // TODO: false if using cloudflare images
     },
 
     experimental: {
