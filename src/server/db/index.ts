c9a5ad78864c9e8bbf3ca5b38bf401a337aa5ce8
Chris Tran <christophertran714@gmail.com>
2024-09-24 18:20:58 -0700

    postgres connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,8 +4,8 @@ import { env } from "@/env";
 import * as schema from "./schema";
 
 export const connection = postgres(env.DATABASE_URL, {
-  max_lifetime: 10, // Remove this line if you're deploying to Docker / VPS
-  // idle_timeout: 20, // Uncomment this line if you're deploying to Docker / VPS
+  // max_lifetime: 10, // Remove this line if you're deploying to Docker / VPS
+  idle_timeout: 20, // Uncomment this line if you're deploying to Docker / VPS
 });
 
 export const db = drizzle(connection, { schema });
